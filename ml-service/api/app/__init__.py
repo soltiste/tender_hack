e91ae884f54bdcf38ee6d0be1ba06166d14eab81
Anton Subbota <asubbota1@its.xyz>
2024-11-09 16:15:50 +0300

    renamed draft

--- ml-service/api/app/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-from api.app.controller import init_routers
-
-def create_app():
-    app = Flask(__name__)
-    init_routers(app)
-    return app
--- ml-service/startup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export PYTHONUNBUFFERED=1
-
-uwsgi --http 0.0.0.0:5000 --wsgi-file main.py --callable app --processes 3
